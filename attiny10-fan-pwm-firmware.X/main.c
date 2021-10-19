922481a7d2f3e7ff03c0a21b7d084dab5f57b6bc
gie_design <zeke@gustelecdesign.com>
2021-10-19 15:43:27 -0500

    Add firmware project

---
+++ attiny10-fan-pwm-firmware.X/main.c
@@ -0,0 +1,15 @@
+/*
+ * File:   main.c
+ * Author: zigmond
+ *
+ * Created on October 19, 2021, 12:44 PM
+ */
+
+
+#include <avr/io.h>
+
+int main(void) {
+    /* Replace with your application code */
+    while (1) {
+    }
+}
